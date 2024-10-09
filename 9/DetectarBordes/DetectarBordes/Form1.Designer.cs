aab8b6b5c99141440e9e8b9eea8b9c30b15390dc
Ronaldo Elvin Yupanqui Salinas <125207330+REYS-NGU@users.noreply.github.com>
2024-10-09 10:31:07 -0400

    Add files via upload

---
+++ 9/DetectarBordes/DetectarBordes/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace DetectarBordes
+{
+    partial class AppDetecBord
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DecBor = new System.Windows.Forms.Button();
+            this.CarIma = new System.Windows.Forms.Button();
+            this.ImagOrig = new System.Windows.Forms.PictureBox();
+            this.ImagWithoutBord = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagOrig)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagWithoutBord)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DecBor
+            // 
+            this.DecBor.Location = new System.Drawing.Point(332, 372);
+            this.DecBor.Name = "DecBor";
+            this.DecBor.Size = new System.Drawing.Size(90, 27);
+            this.DecBor.TabIndex = 0;
+            this.DecBor.Text = "Detectar Borde";
+            this.DecBor.UseVisualStyleBackColor = true;
+            this.DecBor.Click += new System.EventHandler(this.DecBor_Click);
+            // 
+            // CarIma
+            // 
+            this.CarIma.Location = new System.Drawing.Point(222, 372);
+            this.CarIma.Name = "CarIma";
+            this.CarIma.Size = new System.Drawing.Size(90, 27);
+            this.CarIma.TabIndex = 1;
+            this.CarIma.Text = "Cargar Imagen";
+            this.CarIma.UseVisualStyleBackColor = true;
+            this.CarIma.Click += new System.EventHandler(this.CarIma_Click);
+            // 
+            // ImagOrig
+            // 
+            this.ImagOrig.Location = new System.Drawing.Point(12, 53);
+            this.ImagOrig.Name = "ImagOrig";
+            this.ImagOrig.Size = new System.Drawing.Size(300, 300);
+            this.ImagOrig.TabIndex = 2;
+            this.ImagOrig.TabStop = false;
+            // 
+            // ImagWithoutBord
+            // 
+            this.ImagWithoutBord.Location = new System.Drawing.Point(332, 53);
+            this.ImagWithoutBord.Name = "ImagWithoutBord";
+            this.ImagWithoutBord.Size = new System.Drawing.Size(300, 300);
+            this.ImagWithoutBord.TabIndex = 3;
+            this.ImagWithoutBord.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // AppDetecBord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 411);
+            this.Controls.Add(this.ImagWithoutBord);
+            this.Controls.Add(this.ImagOrig);
+            this.Controls.Add(this.CarIma);
+            this.Controls.Add(this.DecBor);
+            this.Name = "AppDetecBord";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ImagOrig)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ImagWithoutBord)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button DecBor;
+        private System.Windows.Forms.Button CarIma;
+        private System.Windows.Forms.PictureBox ImagOrig;
+        private System.Windows.Forms.PictureBox ImagWithoutBord;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ 9/DetectarBordes/DetectarBordes/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DetectarBordes
+{
+    public partial class AppDetecBord : Form
+    {
+        public AppDetecBord()
+        {
+            InitializeComponent();
+        }
+
+        // Función personalizada para reemplazar Math.Clamp
+        private int Clamp(int value, int min, int max)
+        {
+            if (value < min) return min;
+            if (value > max) return max;
+            return value;
+        }
+
+        private void CarIma_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Archivos png|*.png|Archivos jpg|*.jpg";
+            openFileDialog1.ShowDialog();
+            Bitmap bmp = new Bitmap(openFileDialog1.FileName);
+            ImagOrig.Image = bmp;
+        }
+
+        private void DecBor_Click(object sender, EventArgs e)
+        {
+            Bitmap bmp = new Bitmap(ImagOrig.Image);
+            Bitmap result = new Bitmap(bmp.Width, bmp.Height);
+
+            int[,] sX = new int[,]
+    {
+        { -1, 0, 1 },
+        { -2, 0, 2 },
+        { -1, 0, 1 }
+    };
+
+            int[,] sY = new int[,]
+    {
+        { 1, 2, 1 },
+        { 0, 0, 0 },
+        { -1, -2, -1 }
+    };
+
+            int umbral = 180; // Este valor puede ser ajustado para controlar la sensibilidad del borde
+
+            for (int i = 1; i < bmp.Width - 1; i++)
+            {
+                for (int j = 1; j < bmp.Height - 1; j++)
+                {
+                    int pixelX = 0;
+                    int pixelY = 0;
+
+                    for (int k = -1; k <= 1; k++)
+                    {
+                        for (int l = -1; l <= 1; l++)
+                        {
+                            Color pixelColor = bmp.GetPixel(i + k, j + l);
+                            int gris = (pixelColor.R + pixelColor.G + pixelColor.B) / 3;
+
+                            pixelX += gris * sX[k + 1, l + 1];
+                            pixelY += gris * sY[k + 1, l + 1];
+                        }
+                    }
+
+                    int magnitud = (int)Math.Sqrt(pixelX * pixelX + pixelY * pixelY);
+
+                    // Solo mostramos los bordes que superen un cierto umbral
+                    if (magnitud > umbral)
+                    {
+                        magnitud = Clamp(magnitud, 0, 255);
+                        result.SetPixel(i, j, Color.FromArgb(magnitud, magnitud, magnitud));
+                    }
+                    else
+                    {
+                        // Si la magnitud es menor que el umbral, el píxel se pone a negro (no borde)
+                        result.SetPixel(i, j, Color.FromArgb(0, 0, 0));
+                    }
+                }
+            }
+
+            ImagWithoutBord.Image = result;
+        }
+    }
+}
